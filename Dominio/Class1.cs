1b2faba43722a96aa3e734eefef03dc752b3e2ac
Jose Carlos <josecarloslopezm17@gmail.com>
2024-01-18 15:27:51 -0600

    first commit

---
+++ Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Dominio
+{
+    public class Class1
+    {
+    }
+}
---
+++ WebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get() {
+            string[] nombres = new[] {"Jose","Fabian","Saul","David"};
+            return nombres;
+        }
+    }
+}
