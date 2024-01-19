43044af95fecf570c7d9a6721b29fbdc3489b6b7
Jose Carlos <josecarloslopezm17@gmail.com>
2024-01-18 20:54:07 -0600

    Add "Comentario" entity

---
+++ Dominio/Comentario.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Comentario
+    {
+        public int ComentarioId { get; set; }
+        public string Alumno { get; set; }
+        public int Puntaje { get; set; }
+        public string ComentarioTexto { get; set; }
+        public int CursoId { get; set; }
+    }
+}
