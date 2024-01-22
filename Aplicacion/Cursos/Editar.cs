f2c3e77c19967b3963dcc15693841901b9a17a4f
Jose Carlos <josecarloslopezm17@gmail.com>
2024-01-22 12:49:05 -0600

    Add a class to edit courses.

--- Aplicacion/Cursos/Editar.cs
+++ Aplicacion/Cursos/Editar.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
 using Dominio;
@@ -9,10 +8,11 @@ using Persistencia;
 
 namespace Aplicacion.Cursos
 {
-    public class Nuevo
+    public class Editar
     {
         public class Ejecuta : IRequest
         {
+            public int CursoId { get; set; }
             public string Titulo { get; set; }
             public string Descripcion { get; set; }
             public DateTime FechaPublicacion { get; set; }
@@ -20,8 +20,8 @@ namespace Aplicacion.Cursos
 
         public class Manejador : IRequestHandler<Ejecuta>
         {
-            private readonly CursosOnlineContext _context;
 
+            private readonly CursosOnlineContext _context;
             public Manejador(CursosOnlineContext context)
             {
                 _context = context;
@@ -29,21 +29,23 @@ namespace Aplicacion.Cursos
 
             public async Task<Unit> Handle(Ejecuta request, CancellationToken cancellationToken)
             {
-                var curso = new Curso
-                {
-                    Titulo = request.Titulo,
-                    Descripcion = request.Descripcion,
-                    FechaPublicacion = request.FechaPublicacion
-                };
-                _context.Curso.Add(curso);
-                var valor = await _context.SaveChangesAsync();
+                var curso = await _context.Curso.FindAsync(request.CursoId);
 
-                if (valor > 0)
-                {
+                if (curso == null) {
+                    throw new Exception("El curso no existe"); 
+                }
+
+                curso.Titulo = request.Titulo ?? curso.Titulo;
+                curso.Descripcion = request.Descripcion ?? curso.Descripcion;
+                curso.FechaPublicacion = request.FechaPublicacion ?? curso.FechaPublicacion;
+                var resultado = await _context.SaveChangesAsync();
+
+                if (resultado > 0) {
                     return Unit.Value;
                 }
 
-                throw new Exception("No se pudo insertar el curso");
+                throw new Exception("No se guardaron los cambios en el curso");
+
             }
         }
     }
