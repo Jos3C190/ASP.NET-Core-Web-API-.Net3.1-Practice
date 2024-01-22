4afa50697314c1431f7479f31f0d2b5b815bdddb
Jose Carlos <josecarloslopezm17@gmail.com>
2024-01-22 13:18:39 -0600

    Add method in the controller to edit a course

--- WebAPI/Controllers/CursosController.cs
+++ WebAPI/Controllers/CursosController.cs
@@ -35,5 +35,10 @@ namespace WebAPI.Controllers
             return await _mediator.Send(data);
         }
 
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Unit>> Editar(int id, Editar.Ejecuta data) {
+            data.CursoId = id;
+            return await _mediator.Send(data);
+        }
     }
 }
