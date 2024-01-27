a5b37932115bfb66db10f2e5afee8732951257cb
Jose Carlos <josecarloslopezm17@gmail.com>
2024-01-26 20:20:44 -0600

    Add System.IdentityModel.Tokens.Jwt package

--- Seguridad/TokenSeguridad/JwtGenerador.cs
+++ Seguridad/TokenSeguridad/JwtGenerador.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Aplicacion.Contratos;
 using Dominio;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Seguridad.TokenSeguridad
 {
@@ -11,7 +14,25 @@ namespace Seguridad.TokenSeguridad
     {
         public string CrearToken(Usuario usuario)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Mi palabra secreta"));
+            var credenciales = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var TokenDescripcion = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(30),
+                SigningCredentials = credenciales
+            };
+
+            var tokenManejador = new JwtSecurityTokenHandler();
+            var token = tokenManejador.CreateToken(TokenDescripcion);
+
+            return tokenManejador.WriteToken(token);
         }
     }
 }
