38088638fa1d948c848a89ba4d2ec4fdd6549495
Jose Carlos <josecarloslopezm17@gmail.com>
2024-02-03 22:54:55 -0600

    Update CursoId to Guid type and add ListaInstructor property

--- Aplicacion/Cursos/Editar.cs
+++ Aplicacion/Cursos/Editar.cs
@@ -16,14 +16,17 @@ namespace Aplicacion.Cursos
     {
         public class Ejecuta : IRequest
         {
-            public int CursoId { get; set; }
+            public Guid CursoId { get; set; }
             public string Titulo { get; set; }
             public string Descripcion { get; set; }
             public DateTime? FechaPublicacion { get; set; }
+            public List<Guid> ListaInstructor { get; set; }
         }
 
-        public class EjecutaValidacion : AbstractValidator<Ejecuta> {
-            public EjecutaValidacion() {
+        public class EjecutaValidacion : AbstractValidator<Ejecuta>
+        {
+            public EjecutaValidacion()
+            {
                 RuleFor(x => x.Titulo).NotEmpty();
                 RuleFor(x => x.Descripcion).NotEmpty();
                 RuleFor(x => x.FechaPublicacion).NotEmpty();
@@ -42,20 +45,44 @@ namespace Aplicacion.Cursos
             {
                 var curso = await _context.Curso.FindAsync(request.CursoId);
 
-                if (curso == null) {
+                if (curso == null)
+                {
                     throw new ManejadorExcepcion(HttpStatusCode.NotFound, new { mensaje = "No se encontro el curso" });
                 }
 
                 curso.Titulo = request.Titulo ?? curso.Titulo;
                 curso.Descripcion = request.Descripcion ?? curso.Descripcion;
                 curso.FechaPublicacion = request.FechaPublicacion != default ? request.FechaPublicacion : curso.FechaPublicacion;
-                var resultado = await _context.SaveChangesAsync();
 
-                if (resultado > 0) {
-                    return Unit.Value;
+                if (request.ListaInstructor != null)
+                {
+                    if (request.ListaInstructor.Count > 0)
+                    {
+                        var instructoresDB = _context.CursoInstructor.Where(x => x.CursoId == request.CursoId).ToList();
+                        foreach (var instructorEliminar in instructoresDB)
+                        {
+                            _context.CursoInstructor.Remove(instructorEliminar);
+                        }
+
+                        foreach (var ids in request.ListaInstructor)
+                        {
+                            _context.CursoInstructor.Add(new CursoInstructor
+                            {
+                                CursoId = request.CursoId,
+                                InstructorId = ids
+                            });
+                        }
+                    }
                 }
+                    var resultado = await _context.SaveChangesAsync();
 
-                throw new ManejadorExcepcion(HttpStatusCode.InternalServerError, new { mensaje = "Ocurrio un error al actualizar el curso" });
+                    if (resultado > 0)
+                    {
+                        return Unit.Value;
+                    }
+
+                    throw new ManejadorExcepcion(HttpStatusCode.InternalServerError, new { mensaje = "Ocurrio un error al actualizar el curso" });
+
 
             }
         }
